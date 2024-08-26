3e59d5eeebf45f27e395c5bb00f5103d951e2a8e
guilopes2020 <guilopesdev@gmail.com>
2024-08-26 18:33:06 -0300

    feat: :sparkles: methods

---
+++ Calculadora.cs
@@ -0,0 +1,23 @@
+// namespace treinee_c_sharp
+public class Calculadora
+{
+    public static int calcularSoma(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+
+    public static int calcularSubtracao(int num1, int num2)
+    {
+        return num1 - num2;
+    }
+
+    public static int calcularMultiplicacao(int num1, int num2)
+    {
+        return num1 * num2;
+    }
+
+    public static int calcularDivisao(int num1, int num2)
+    {
+        return num1 / num2;
+    }
+}
